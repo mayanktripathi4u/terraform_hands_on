c3502c0aecee7f43409199a08fad7246f76a94a1
Mayank Tripathi <31957614+mayanktripathi4u@users.noreply.github.com>
2023-12-28 15:43:12 -0600

    Update main.tf

--- generic/basics/01/main.tf
+++ generic/basics/01/main.tf
@@ -1,3 +1,10 @@
 provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws-instance" "example" {
+  ami = "ami-053..."
+  instance_type = "t2.micro"
+  subnet_id = "subnet_id"
+  key_name = "keypair"
 }
